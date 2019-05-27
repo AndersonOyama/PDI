c47da4381b4d4e1059eac7ff3641c6308ad87247
AndersonOyama <anderson.oyama@hotmail.com>
2019-05-27 18:37:43 -0300

    Update

--- pdi.py
+++ pdi.py
@@ -3,6 +3,12 @@ import math
 import cv2
 import argparse
 
+COLOR_SPACE = 255
+RED = []
+GREEN = []
+BLUE = []
+
+
 parser = argparse.ArgumentParser(description='Get file directory')
 parser.add_argument('file', help='file directory', type=str)
 parser.add_argument('num_div', help='number of division', type=int)
@@ -20,29 +26,32 @@ def partition_mul(n):
     		d += 1
     while len(div) > 3:
         temp = div[0]*div[1]
-        # print(temp)
         del div[0]
         del div[0]
         div.append(temp)
         div.sort()
-        # print(len(div))
+    if(len(num) < 3 and len(num) >= 2):
+        num.append(1)
+    elif (len(num)<=1 and len(num)>=0):
+        num.append(1)
+        num.append(1)
     return div
 
 
-
-
-
+def distance_color():
+    a = 0
 
 img = cv2.imread(args.file, 1)
 num = []
 num = partition_mul(args.num_div)
-if(len(num) < 3 and len(num) >= 2):
-    num.append(1)
-elif (len(num)<=1 and len(num)>=0):
-    num.append(1)
-    num.append(1)
+
 print(num)
 
+RED.append(COLOR_SPACE//(num[0]+1))
+GREEN.append(COLOR_SPACE//(num[1]+1))
+BLUE.append(COLOR_SPACE//num[2]+1)
+
+print(RED)
 # cv2.imshow('imagem', img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
